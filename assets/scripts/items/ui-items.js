8ad2b32ebc03b51de2dfb9d611f0f1599de56a1a
mgreim3373 <mgreim3373@gmail.com>
2018-09-20 14:58:34 -0400

    Added autofill to edit form -edit form now auto populates with prefilled information

--- assets/scripts/items/ui-items.js
+++ assets/scripts/items/ui-items.js
@@ -1,4 +1,5 @@
 'use strict'
+const store = require('../store.js')
 
 const ItemTemplate = require('../../templates/list.handlebars')
 
@@ -15,6 +16,7 @@ const showItemsSuccess = function (data) {
   const selectItemHtml = ItemTemplate({ items: data.items })
   $('#bucket-list-content').html(selectItemHtml)
   $('#bucket-list-display').removeClass('hide')
+  $('.edit-button').on('click', fillEditInputs)
 
   console.log('information returned', data)
 }
@@ -29,6 +31,21 @@ const deleteItemSuccess = function () {
 
 const deleteItemFail = function () {
   alert('failed to delete item')
+}
+
+const fillEditInputs = function (event) {
+  let itemId = $(event.target).closest('tbody').data('id')
+  store.id = itemId
+  let itemName = $(event.target).closest('tbody').data('name')
+  let itemDescription = $(event.target).closest('tbody').data('description')
+  let itemLocation = $(event.target).closest('tbody').data('location')
+  let itemPriority = $(event.target).closest('tbody').data('priority')
+  let itemStatus = $(event.target).closest('tbody').data('status')
+  console.log(itemName, itemDescription)
+  $('#editItem input[name="items[name]"]').val(itemName)
+  $('#editItem input[name="items[description]"]').val(itemDescription)
+  $('#editItem input[name="items[location]"]').val(itemLocation)
+  $('#editItem input[name="items[priority]"]').val(itemPriority)
 }
 
 module.exports = {
